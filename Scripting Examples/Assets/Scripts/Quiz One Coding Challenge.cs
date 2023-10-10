2ead5491f963e6a81a51483c6e9937cfd3e34be7
Lounge101 <noahtoledo170@riversideunified.org>
2023-10-10 14:01:40 -0700

    Update Code Challenge

---
+++ Scripting Examples/Assets/Scripts/Quiz One Coding Challenge.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizOneCodingChallenge : MonoBehaviour
+{
+    public int score = 0;
+
+    public string playerName = "Noah";
+
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // method for PrintPlayerName
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // method to increaseScore
+
+    }
+}
